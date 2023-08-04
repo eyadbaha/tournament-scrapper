f132e8ebd46fe70fda8649fc77dc8f02d5f8353f
eyadbaha <yuseifudoegypt@gmail.com>
2023-08-04 09:37:00 +0300

    startgg info route added

--- src/routes/startgg.ts
+++ src/routes/startgg.ts
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 
 const startggRouter = express.Router();
+const token = process.env.STARTGG_TOKEN;
 startggRouter.get("/brackets/:id", async (req: any, res: any) => {
   const eventId = req.params.id;
   const phasesRequest: any = await axios.post(
@@ -21,7 +22,7 @@ startggRouter.get("/brackets/:id", async (req: any, res: any) => {
         eventId: eventId,
       },
     },
-    { headers: { Authorization: "Bearer bd7ea184d202fed6edbb7d21d7c87692" } }
+    { headers: { Authorization: `Bearer ${token}` } }
   );
   const phases = phasesRequest.data.data.tournament.events[0].phases;
 
@@ -58,25 +59,55 @@ startggRouter.get("/brackets/:id", async (req: any, res: any) => {
           eventId: eventId,
         },
       },
-      { headers: { Authorization: "Bearer bd7ea184d202fed6edbb7d21d7c87692" } }
+      { headers: { Authorization: `Bearer ${token}` } }
     );
     res.end(
       JSON.stringify(
-        dataRequest.data.data.tournament.events[0].phases[0].sets.nodes.map(
-          (set: any) => {
-            const player1 = [
-              set.slots[0].standing.entrant.id,
-              set.slots[0].standing.stats.score.value,
-            ];
-            const player2 = [
-              set.slots[1].standing.entrant.id,
-              set.slots[1].standing.stats.score.value,
-            ];
-            return { round: set.round, player1, player2 };
-          }
-        )
+        dataRequest.data.data.tournament.events[0].phases[0].sets.nodes.map((set: any) => {
+          const player1 = [set.slots[0].standing.entrant.id, set.slots[0].standing.stats.score.value];
+          const player2 = [set.slots[1].standing.entrant.id, set.slots[1].standing.stats.score.value];
+          return { round: set.round, player1, player2 };
+        })
       )
     );
   }
+});
+startggRouter.get("/info/:id", async (req: any, res: any) => {
+  const eventId = req.params.id;
+  const infoRequest: any = await axios.post(
+    "https://api.start.gg/gql/alpha",
+    {
+      query: `query tournament($eventId: String!) {
+        tournament(slug: $eventId) {
+          name
+          numAttendees
+          rules
+          events(limit:1) {
+            state
+            startAt
+            videogame {
+              name
+            }
+          }
+        }
+      }`,
+      variables: {
+        eventId: eventId,
+      },
+    },
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
+  const response = infoRequest?.data?.data?.tournament;
+  const [title, date, details, game, participants] = [
+    response.name,
+    response.events[0].startAt * 1000,
+    response.rules,
+    response.events[0].videogame.name,
+    response.numAttendees,
+  ];
+  const statusMap: { [key: string]: number } = { CREATED: 0, ACTIVE: 1, COMPLETED: 2 };
+  const stateText = response.events[0].state as string;
+  const state: number = statusMap[stateText] || -1;
+  res.send(JSON.stringify({ title, date, details, game, participants, state }));
 });
 export default startggRouter;
