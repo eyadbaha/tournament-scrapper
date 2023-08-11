595b84601b3ac61dce704b6207dd533789a30bf1
eyadbaha <yuseifudoegypt@gmail.com>
2023-08-11 11:47:33 +0300

    padiagaming refactor

--- src/routes/padiagaming.ts
+++ src/routes/padiagaming.ts
@@ -16,10 +16,7 @@ padiaGamingRouter.get("/info/:id", async (req, res) => {
   });
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await Promise.all([
-    page.goto(`https://paidiagaming.com/tournament/${id}/overview`),
-    page.waitForNavigation({ waitUntil: "networkidle0" }),
-  ]);
+  await page.goto(`https://paidiagaming.com/tournament/${id}/overview`, { waitUntil: "networkidle0" });
   const data = await page.evaluate(() => {
     const date = new Date(
       document.querySelector(".UpcomingSlider-subtitle")?.textContent?.replace(/ •.*/, "") ?? ""
@@ -28,18 +25,17 @@ padiaGamingRouter.get("/info/:id", async (req, res) => {
       .querySelector(".UpcomingSlider-subtitle")
       ?.textContent?.match(/[^•]*\s*•\s*(\S.*\S)\s*$/)?.[1];
     const title = document.querySelector(".UpcomingSlider-title")?.textContent?.trim();
-    const details = (document.querySelector(".Overview-information-desc") as HTMLElement)?.innerText;
+    const details = document.querySelector<HTMLElement>(".Overview-information-desc")?.innerText;
     const participants = Number(
       document.querySelector(".viewtournament-participent")?.textContent?.match(/.*\((.*)\).*/)?.[1]
     );
-    var xpathResult = document.evaluate(
+    const span = document.evaluate(
       "//span[.//span[contains(text(), 'End time')] and contains(@class, 'text_part')]",
       document,
       null,
       XPathResult.FIRST_ORDERED_NODE_TYPE,
       null
-    );
-    const span = xpathResult.singleNodeValue as HTMLElement | undefined;
+    )?.singleNodeValue as HTMLElement | undefined;
     const endTime = new Date(span?.querySelector(".text_date")?.textContent || "").getTime();
     const timeNow = new Date().getTime();
     let state = 0;
@@ -61,65 +57,45 @@ padiaGamingRouter.get("/brackets/:id", async (req, res) => {
     headless: chromium.headless,
   });
   const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0);
   await Promise.all([
     page.goto(`https://paidiagaming.com/tournament/${id}/brackets`),
-    page.waitForSelector(".Match-wrapper", { timeout: 70000 }),
+    page.waitForSelector(".Match-wrapper"),
   ]);
   const tournamentType = await page.evaluate(() => {
-    const elements = document.querySelectorAll("p.Play-slider-detail-info");
-    for (let element of elements) {
-      if ((element as HTMLElement).innerText.includes("Tournament type")) {
-        return (element.querySelector(".Play-slider-detail-text") as HTMLElement).innerText;
-      }
+    for (const element of document.querySelectorAll<HTMLElement>("p.Play-slider-detail-info")) {
+      if (element?.innerText.includes("Tournament type"))
+        return element.querySelector<HTMLElement>(".Play-slider-detail-text")?.innerText;
     }
   });
-  if (tournamentType?.includes("Single Elimination")) {
-    const roundExpantionButtion = await page.$(".mat-select-placeholder");
-    await Promise.all([
-      roundExpantionButtion?.click(),
-      page.waitForSelector(".mat-select-search-panel", { visible: true }),
-    ]);
-    const roundList = await page.$(".mat-select-search-panel");
-    const roundOptions = await roundList?.$$("mat-option");
-    const grandFinalsButton = roundOptions?.[roundOptions?.length - 1];
-    const initialContent = await page.$eval(".SingleElimination-round", (el) => el.textContent);
-    await grandFinalsButton?.click();
-    await page.waitForFunction(
-      (initialContent) => {
-        const currentContent = document.querySelector(".SingleElimination-round")?.textContent;
-        return currentContent !== initialContent;
-      },
-      {},
-      initialContent
-    );
-    const brackets = await page.evaluate(() => {
-      const matches: any[] = [];
-      const rounds = document.querySelectorAll(".SingleElimination-round");
-      rounds.forEach((round, roundIndex) => {
-        const roundMatches = round.querySelectorAll(".Match-wrapper");
-
-        roundMatches.forEach((match) => {
-          const players = match.querySelectorAll("lib-team");
-
-          const matchPlayers: any[] = [];
-          players.forEach((player) => {
-            const name = player.querySelector(".BracketMatch-items-player")?.textContent?.replace(/ /g, "");
-            const score = player.querySelector(".BracketMatch-items-score")?.textContent?.replace(/ /g, "");
-            matchPlayers.push({ id: name, score: score });
-          });
-          matches.push({ round: roundIndex, players: matchPlayers });
-        });
-      });
-      return matches.reverse();
-    });
-    return res.send(JSON.stringify(brackets));
-  }
-  return res.send(
-    JSON.stringify({
+  if (!tournamentType?.includes("Single Elimination"))
+    return res.json({
       message: "Bracket fetching is only supported for Single Elimination Tournaments",
-    })
-  );
+    });
+  const roundExpantionButtion = await page.$(".mat-select-placeholder");
+  await Promise.all([
+    roundExpantionButtion?.click(),
+    page.waitForSelector(".mat-select-search-panel", { visible: true }),
+  ]);
+  await page.click(".mat-select-search-panel mat-option:last-child");
+  await page.waitForFunction(() => document.querySelectorAll(".Match").length === 7);
+  const brackets = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".SingleElimination-round"))
+      .map((round, roundIndex) => {
+        return Array.from(round.querySelectorAll(".Match-wrapper")).map((match) => {
+          return {
+            round: roundIndex,
+            players: Array.from(match.querySelectorAll("lib-team")).map((player) => {
+              return {
+                id: player.querySelector(".BracketMatch-items-player")?.textContent?.trim(),
+                score: player.querySelector(".BracketMatch-items-score")?.textContent?.trim(),
+              };
+            }),
+          };
+        });
+      })
+      .flat()
+      .reverse();
+  });
+  return res.json(brackets);
 });
-
 export default padiaGamingRouter;
