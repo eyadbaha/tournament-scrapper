54d728b29e350feceee13cedada2a3b2c7219a2c
eyadbaha <yuseifudoegypt@gmail.com>
2023-10-06 03:21:02 +0300

    Add minutes to date formatter

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -9,11 +9,12 @@ const formatDate = (timestamp: number) => {
   const month = date.getMonth() + 1; // Months are zero-based, so add 1
   const year = date.getFullYear();
   const hour = date.getHours();
+  const minute = date.getMinutes(); // Get the minutes
   const ampm = hour >= 12 ? "PM" : "AM";
   const hour12 = hour % 12 || 12; // Convert to 12-hour format
 
   // Format the components into the desired string
-  const formattedDate = `${day}/${month}/${year} ${hour12}${ampm} (UTC+3)`;
+  const formattedDate = `${day}/${month}/${year} ${hour12}:${minute < 10 ? "0" : ""}${minute}${ampm} (UTC+3)`;
   return formattedDate;
 };
 export default formatDate;
