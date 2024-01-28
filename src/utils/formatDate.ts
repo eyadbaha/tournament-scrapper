53e756b919d5c39c065ee8f20776b82d3bd1defd
eyadbaha <yuseifudoegypt@gmail.com>
2024-01-28 05:59:42 +0200

    changed time zone to UTC+2

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,8 +1,8 @@
 const formatDate = (timestamp: number) => {
   const date = new Date(timestamp);
 
-  // Set the time zone offset to UTC+3 (180 minutes ahead of UTC)
-  date.setMinutes(date.getMinutes() + 180);
+  // Set the time zone offset to egypt time zone
+  date.setMinutes(date.getMinutes() + 120);
 
   // Get the individual components (date, month, year, hour, minute) from the date
   const day = date.getDate();
@@ -14,7 +14,7 @@ const formatDate = (timestamp: number) => {
   const hour12 = hour % 12 || 12; // Convert to 12-hour format
 
   // Format the components into the desired string
-  const formattedDate = `${day}/${month}/${year} ${hour12}:${minute < 10 ? "0" : ""}${minute}${ampm} (UTC+3)`;
+  const formattedDate = `${day}/${month}/${year} ${hour12}:${minute < 10 ? "0" : ""}${minute}${ampm} (UTC+2)`;
   return formattedDate;
 };
 export default formatDate;
